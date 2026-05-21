b65c6e3bcf27f85819fba32e48090bc0bf1280bb
frostkom <martin.fors@frostkom.se>
2026-05-21 09:43:10 +0200

    Remove new emoji styles action

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Cache
 Plugin URI: https://github.com/frostkom/mf_cache
 Description: Add options to cache pages for better performance
-Version: 4.14.0
+Version: 4.14.1
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
@@ -74,8 +74,8 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 		remove_action('wp_print_styles', 'wp_enqueue_emoji_styles');
 		remove_action('admin_print_scripts', 'print_emoji_detection_script');
 		remove_action('admin_print_styles', 'wp_enqueue_emoji_styles');
-		remove_action('wp_print_styles', 'print_emoji_styles');
-		remove_action('admin_print_styles', 'print_emoji_styles');
+		remove_action('wp_print_styles', 'wp_enqueue_emoji_styles');
+		remove_action('admin_print_styles', 'wp_enqueue_emoji_styles');
 		remove_filter('the_content_feed', 'wp_staticize_emoji');
 		remove_filter('comment_text_rss', 'wp_staticize_emoji');
 		remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
